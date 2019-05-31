ef6820095151a6497007626d4f76e4ee8cc4fca7
Eclesio Ferreira <eclesio@alphasoftware.br>
2019-05-31 14:49:23 -0400

    Initilizing travis.yml configurations

--- creational/abstract-factory/php/app/factory/OReillyBookFactory.php
+++ creational/abstract-factory/php/app/factory/OReillyBookFactory.php
@@ -4,13 +4,15 @@ namespace App\Factory;
 use App\Models\OReillyPHPBook;
 use App\Models\OReillyNodeJsBook;
 
+use App\Models\SamsPHPBook;
+
 /** Implementations of AbstractBookFactory */
 class OReillyBookFactory extends AbstractBookFactory {
     private $context = "OReilly";
     
     /** Return a new OReillyPHPBook instance */
     function makePHPBook() {
-        return new OReillyPHPBook();
+        return new SamsPHPBook();
     }
 
     /** Return a new OReillyNodeJsBook instance */
